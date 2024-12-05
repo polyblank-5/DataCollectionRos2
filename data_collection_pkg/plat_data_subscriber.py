7455765c2340c9ca9ba8bfe86ae553b073cd0d56
Jan Achermann <achermann@campus.tu-berlin.de>
2024-12-05 18:47:50 +0100

    fix publisher(String, small fixes)

--- data_collection_pkg/plat_data_subscriber.py
+++ data_collection_pkg/plat_data_subscriber.py
@@ -1,73 +1,75 @@
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg import Float32MultiArray  # Replace with the appropriate message types for your topics
+from std_msgs.msg import Float32MultiArray, String  # Replace with the appropriate message types for your topics
 import pygame
 import yaml
 import os
 from typing import List, Tuple
 import math
 from copy import deepcopy
+import json
 type Position = List[float]
 
 class PlantDataSubscriber(Node):
     def __init__(self):
-        super().__init__('dual_subscriber')
+        super().__init__('plant_data_subscriber')
         
-        package_share_directory = os.path.join(
-            os.getenv('AMENT_PREFIX_PATH').split(':')[0], 'share/data_collection_pkg/config')
-        config_file_path = os.path.join(package_share_directory, 'config.yaml')
+        os.chdir('src/data_collection_pkg')
+        #package_share_directory = os.path.join(    os.getenv('AMENT_PREFIX_PATH').split(':')[0], 'share/data_collection_pkg/config')
+        config_file_path = os.path.join('config/', 'config.yaml')
     
         with open(config_file_path, 'r') as file:
                 config = yaml.safe_load(file)
+
         constants = config['constants']
         
-        self.plant_velocity:float = None
-        self.plant_rotation:float = None
+        self.plant_velocity:float = 0.0
+        self.plant_rotation:float = 0.0
         self.plant_positions:List[Position] = []
 
         self._CELL_WIDTH = constants['SCREEN_WIDTH'] // int(constants['FRAME_WIDTH'] / constants['FRAME_DISCRETIZATION'])
         self._CELL_HEIGHT = constants['SCREEN_HEIGHT'] // int(constants['FRAME_WIDTH'] / constants['FRAME_DISCRETIZATION'])
         
-        self.publisher_ = self.create_publisher(Float32MultiArray, 'plant_data_publisher', 10)
+        self.publisher_ = self.create_publisher(String, 'plant_data_publisher', 10)
         # Subscription to the first topic
-        self.subscription_1 = self.create_subscription(
-            Float32MultiArray,             # Replace 'String' with your topic's message type
+        self.plant_position_subscription = self.create_subscription(
+            String,             # Replace 'String' with your topic's message type
             'plant_position_publisher',          # Replace 'topic_1' with the name of your first topic
             self.callback_plant_position_subscriber,    # Callback for the first topic
             10                  # QoS depth
         )
-        self.subscription_1  # Prevent unused variable warning
+        self.plant_position_subscription  # Prevent unused variable warning
         
         # Subscription to the second topic
-        self.subscription_2 = self.create_subscription(
+        self.plant_velocity_subscription = self.create_subscription(
             Float32MultiArray,             # Replace 'String' with your topic's message type
             'plant_velocity_publisher',          # Replace 'topic_2' with the name of your second topic
             self.callback_plant_velocity_subscriber,    # Callback for the second topic
             10                  # QoS depth
         )
-        self.subscription_2  # Prevent unused variable warning
+        self.plant_velocity_subscription  # Prevent unused variable warning
 
     # Callback for the first topic
     def callback_plant_position_subscriber(self, msg):
-        self.get_logger().info(f'Received from {self.subscription_1.topic_name}: {msg.data}')
-        new_postions = msg.data
+        self.get_logger().info(f'Received from {self.plant_position_subscription.topic_name}: {msg.data}')
+        new_postions = json.loads(msg.data)
         if self.plant_positions == []:
             self.plant_positions = new_postions
         else:
-            for positions in self.plant_positions:
-                positions[0], positions[1] = self.update_position(positions[0], positions[1])
+            for i,positions in enumerate(self.plant_positions):
+                self.plant_positions[i][0], self.plant_positions[i][1] = self.update_position(positions[0], positions[1])
             
             self.plant_positions = self.find_closest_points(self.plant_positions,new_postions)
-        publisher_msg = Float32MultiArray()
-        publisher_msg = deepcopy(self.plant_positions)
+        publisher_msg = String()
+        publisher_msg.data = json.dumps(self.plant_positions)
         self.publisher_.publish(publisher_msg)
-        self.get_logger().info(f'Publishing Data to {self.publisher_.topic_name}: {msg.data}')
+        self.get_logger().info(f'Publishing Data to {self.publisher_.topic_name}: {publisher_msg.data}')
 
     # Callback for the second topic
     def callback_plant_velocity_subscriber(self, msg):
-        self.plant_velocity = msg.data[0]
-        self.plant_rotation = msg.data[1]
-        self.get_logger().info(f'Received from topic_2: {msg.data}')
+        self.plant_velocity = float(msg.data[0])
+        self.plant_rotation = float(msg.data[1])
+        self.get_logger().info(f'Received from {self.plant_velocity_subscription.topic_name}: {msg.data}')
 
     def update_position(self,x:float,y:float) -> Tuple[float,float]:
         x += self.plant_velocity
